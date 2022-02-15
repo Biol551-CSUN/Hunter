b7f2868c21ab1e8d92636bdf6516f62376975fab
tinyjessica <tinkerbell404@gmail.com>
2022-02-15 12:13:12 -0800

    adding a notes section for myself

---
+++ notes/notes.R
@@ -0,0 +1,36 @@
+### this is my notes section for myself
+
+
+################## Transparent Script #######
+## 1. Intro - name date last edir
+
+## 2. Libraries
+
+## 3. function
+
+## 4. Load data
+
+## 5. Analysis
+
+## comment comment comment
+
+
+
+###### Daily ######
+#git pull
+#git add testscript.r
+##git commit -a -m "I am adding testscrip.R"
+#git push
+
+
+##### ggplot ######
+#ggsave(here("week_3", "output", "grouphomework4.png"),
+#       width = 7, height = 5)
+
+
+
+
+
+##### random ####
+### use gg genes to create genes
+
