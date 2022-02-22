d6fedbd4435901d882308cf99bce15eea739d379
tinyjessica <tinkerbell404@gmail.com>
2022-02-22 12:49:58 -0800

    Updated Thursday homework script outputs

--- week_4/scripts4/w4ThursdayLab.R
+++ week_4/scripts4/w4ThursdayLab.R
@@ -2,6 +2,7 @@
 #### Week 4 Thursday Script for Lab
 ### Author Group 1 and Jessica Hunter
 ### Created February 17, 2022
+### updated February 22, 2022
 
 # This script will filter the data and make a plot and output it
 ###############################################
@@ -21,11 +22,15 @@ view(chemdata2)
 
 #### Functions ####
 
+## ------- This function creates a clean script without any NA's
+## it filters out the Night data, it pivots my data longer, 
+## calculates the average water composition by season and Site location 
+## and creates a csv file of my new data summary
 chemdata2clean<-chemdata2 %>% # creating new file of clean data
   filter(complete.cases(.)) %>%  # removes the columns that are incomplete
   separate(col = Tide_time, # this is the colume I want to seperate
            into = c("tide", "time"), # these are titles for the new data columns
-           sep = "_",
+           sep = "_", # separates using an _ underscore symbol
            remove = FALSE) %>% # true deletes original col and false keeps the original col
   filter(time == "Night") # Only kept night data
 ### 
@@ -41,19 +46,33 @@ view(chemdatalong)
 chemdatalong %>% 
   group_by(variables,Season, Site) %>% 
   summarise(mean_value = mean(values, na.rm = TRUE)) %>% 
-  write_csv(here("week_4" , "output4", "Summary_composition_at_night"))
+  write_csv(here("week_4" , "output4", "Summary_composition_at_night.csv"))
 
 
 
-chemdata2clean %>% 
-  ggplot(aes(x= Season,
-             y = Temp_in, fill=tide))+
-           geom_violin()
+#################### Function 2 ------------
+### this function creates a violin plot of the water composition values based on high and low tide
+
+
 
 chemdatalong %>% 
   ggplot(aes(x= variables,
-             y = values, fill=tide))+
-  geom_violin()+
-  facet_wrap(~variables,
-             scales = "free")
-  
+             y = values, 
+             fill=tide))+
+  geom_violin()+ # vilin plot
+  facet_wrap(~variables, # wraps by columns
+             scales = "free")+ # releases the scales to be different for each mini plot
+
+labs(y = "Values",
+     x= "Water Composition Variables",
+     tide = "Tide",
+     title = "The Composition of Water during High Tide and Low Tide", # created title to plot
+     caption = "Source: Silbinger et al. 2020 Proceedings of the Royal Society:B")+ # add a caption ;source
+  theme(plot.title = element_text(size = 20), # changes the size of the title
+        panel.background = element_rect(fill="linen"), # changes the background on the plot
+        axis.title = element_text(size = 15))  # change the size of the axis titles
+        # legend.text = element_text("Tide"))
+ggsave(here("week_4", "output4", "Thursday_lab_homework.png"), ## saving to my folder output
+       width = 7,
+       height = 5) # in inches
+
